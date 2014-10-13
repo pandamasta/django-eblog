6f18d3a00e776cf972f814fbf6ce50ec4b7cf85e
Aurelien Martin <01aurelien@gmail.com>
2014-10-13 16:27:06 +0200

    Change the method get_query_set() to get_queryset() as required by django 1.7

--- eblog/managers.py
+++ eblog/managers.py
@@ -9,10 +9,10 @@ class CategoryOnlineManager(models.Manager):
     Manager that manages online ``Category`` objects.
     """
 
-    def get_query_set(self):
+    def get_queryset(self):
         from blog.models import Entry
         entry_status = Entry.STATUS_ONLINE
-        return super(CategoryOnlineManager, self).get_query_set().filter(
+        return super(CategoryOnlineManager, self).get_queryset().filter(
             entry__status=entry_status).distinct()
 
 
@@ -21,7 +21,7 @@ class EntryOnlineManager(models.Manager):
     Manager that manages online ``Entry`` objects.
     """
 
-    def get_query_set(self):
-        return super(EntryOnlineManager, self).get_query_set().filter(
+    def get_queryset(self):
+        return super(EntryOnlineManager, self).get_queryset().filter(
             status=self.model.STATUS_ONLINE)
 
